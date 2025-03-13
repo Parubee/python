885843e4f8eaad4c56dabb4c20c30c72bf7a87f4
Paravthi KB <127673281+Parubee@users.noreply.github.com>
2025-03-13 19:03:37 +0530

    Add files via upload

---
+++ mod4.1.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__ (self ,name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("john","36")
+p2 = Person("Mariya","42")
+
+p1 = print (p1.name,p1.age)
+p2 =  print(p2.name)
+    
